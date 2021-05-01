ea638d572bba43ce535b4f0bd05a6fbdb3c3fd76
meetparag81 <paragborawake81@gmail.com>
2021-05-02 00:09:27 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpIMP
+{
+    class Program
+    {
+        private static string[] pathtest;
+
+        static void Main(string[] args)
+        {
+            string path = GetResourcePath("Testdata\\Book1.xlsx");
+            Console.WriteLine(path);
+        }
+        public static String GetResourcePath(String path)
+        {
+
+            String BasePath = System.IO.Directory.GetCurrentDirectory();
+            if (BasePath.Contains("bin")){
+                string[] stringSeparators = new String[] { "bin" };
+                string[] test1 = BasePath.Split(stringSeparators, StringSplitOptions.None);
+                BasePath = test1[0].ToString();
+                
+
+
+            }
+
+            
+            Console.WriteLine("Base Src folder location is"+ BasePath);
+            return BasePath + path;
+
+
+        }
+    }
+}
