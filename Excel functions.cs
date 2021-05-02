408eac4236e35d772a8608445c8fb188fd2c9df5
meetparag81 <paragborawake81@gmail.com>
2021-05-02 14:13:48 +0530

    modified package configuration

--- Excel functions.cs
+++ Excel functions.cs
@@ -8,5 +8,21 @@ namespace CsharpIMP
 {
     class Excel_functions
     {
+        public void openexcel()
+        {
+            xlapp = new xl.Application();
+            workbooks = xlapp.Workbooks;
+            object xlFilepath = null;
+            workbook = workbooks.Open(xlFilepath);
+            //storing workshetnames in hashtable
+            int count = 1;
+            foreach (xl.Worksheet sheet in workbook.Sheets)
+            {
+                sheets[count] = sheet.Name;
+                count++;
+            }
+
+        }
+
     }
 }
---
+++ ResourceHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace selenium_practice.Resource
+{
+    class ResourceHelper
+    {
+        public static String GetResourcePath(String path)
+        {
+            String BasePath = System.IO.Directory.GetCurrentDirectory();
+            if (BasePath.Contains("bin"))
+            {
+                string[] stringSeparators = new String[] { "bin" };
+                string[] test1 = BasePath.Split(stringSeparators, StringSplitOptions.None);
+                BasePath = test1[0].ToString();
+
+
+
+            }
+            Console.WriteLine("Base Src folder location is" + BasePath);
+            Console.WriteLine("Testdatalocation is" + BasePath+ path);
+            return BasePath + path;
+
+
+        }
+    }
+}
